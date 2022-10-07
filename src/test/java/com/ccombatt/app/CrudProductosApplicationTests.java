e3f43ecb6449ff0f0d3ec6bdba56d7588bbe2324
Junior-Lasprilla <lx12268@innovaccion.mx>
2022-10-07 15:04:55 -0500

    Se agregan los archivos necesarios para el proyecto

---
+++ src/test/java/com/ccombatt/app/CrudProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccombatt.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
